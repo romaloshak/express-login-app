92f4eb8e338c5e93ed3b9719333819a7d5296c12
Roma Loshak <sunnyboy7538@gmail.com>
2026-03-14 16:32:01 +0400

    add migrations

---
+++ migrations/1773490954477_create-users-table.ts
@@ -0,0 +1,20 @@
+import type { ColumnDefinitions, MigrationBuilder } from 'node-pg-migrate';
+
+export const shorthands: ColumnDefinitions | undefined = undefined;
+
+export async function up(pgm: MigrationBuilder): Promise<void> {
+	pgm.sql(`
+    CREATE TABLE users (
+      id SERIAL PRIMARY KEY,
+      username VARCHAR(50) NOT NULL,
+      email VARCHAR(100) UNIQUE NOT NULL,
+      password VARCHAR(100) NOT NULL,
+      refresh_token TEXT,
+      created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    );
+  `);
+}
+
+export async function down(pgm: MigrationBuilder): Promise<void> {
+	pgm.sql(`DROP TABLE users;`);
+}
